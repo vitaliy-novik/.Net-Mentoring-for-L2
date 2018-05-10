c67a1fbbda6dc03fad4f4ec572658a95453c5201
Vitali_Novik <vitaliy.novik.94@gmail.com>
2018-05-10 03:14:46 +0300

    debug

---
+++ Debugging/KeyGen/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace KeyGen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+			var addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+			var date = DateTime.Now.Date;
+			var dateBytes = BitConverter.GetBytes(date.ToBinary());
+			var source = addressBytes
+				.Select((b, i) => b ^ dateBytes[i])
+				.Select(e => e * 10)
+				.ToArray();
+
+			Console.WriteLine(string.Join("-", source.ToArray()));
+			Console.ReadKey();
+		}
+	}
+}
